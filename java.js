981d899b3837bd3915ce4172ec86763703898234
just-in-and-out <54847960+just-in-and-out@users.noreply.github.com>
2020-01-09 09:06:14 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+console.log("hallo world");
+
+for (var i = 0; i < 11; i++) {
+  console.log("de waarde van i is "+ i);
+}
+
+var mijnauto = {
+  merk: "Ford",
+  type: "mondeo",
+  aantalwielen: 4,
+  kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
